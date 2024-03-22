695bd9fc6a2a6d07cff1624da5db197cb62bde7c
Lucila Amand de Mendieta <amanddemendietalucila@gmail.com>
2024-03-22 17:27:00 +0100

    mi primer commit

---
+++ js/ej_1_2_3.js
@@ -0,0 +1,16 @@
+//1-
+alert ("un mensaje");
+
+//2-
+document.write("Hello World")
+
+//3-
+document.write("3+5")
+
+//4-
+let nombre = prompt("Ingrese su nombre")
+document.write("Hola ", nombre)
+
+
+
+
---
+++ js/ej_4.js
@@ -0,0 +1,5 @@
+
+//5-
+let primerNumero = prompt ("Ingrese un numero")
+let segundoNumero = prompt ("Ingrese un numero")
+document.write ("Su primer numero es "+primerNumero+" y su segurndo numero es "+segundoNumero+" y la suma es "+(parseInt(primerNumero)+(segundoNumero)))
